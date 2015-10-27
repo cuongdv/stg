b461a82e343fbd046bb71f5b4045e07e3f7e0da7
Yohei Washizaki <yohei.washizaki@gmail.com>
2015-10-27 22:58:01 +0900

    戦闘機追加

---
+++ stg00/Assets/EagleGames.jp/Scripts/PlCtrl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlCtrl : MonoBehaviour
+{
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var dir = Vector3.zero;
+
+		if (Input.GetKey(KeyCode.UpArrow) == true)
+		{
+			dir.z = 1f;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow) == true)
+		{
+			dir.z = -1f;
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow) == true)
+		{
+			dir.x = -1f;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow) == true)
+		{
+			dir.x = 1f;
+		}
+
+		transform.Translate(dir.normalized * Time.deltaTime * speed);
+	}
+}
