d48e73ba29e229e9d6c12f13a08b23a6277b0f98
Yohei Washizaki <yohei.washizaki@gmail.com>
2015-10-28 22:48:27 +0900

    水面追加

---
+++ stg00/Assets/EagleGames.jp/Scripts/Pl/P47Animation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P47Animation : MonoBehaviour
+{
+	public Transform propeller;
+	public Vector3 axis = Vector3.up;
+	public float angle = 10f;
+
+
+	public Transform rootNode;
+	[Range(-1f, 1f)]
+	public float roll = 0f;
+
+	/// <summary>
+	/// 入力がなくなった時の水平に復元しようとする速度
+	/// </summary>
+	public float speed = 2f;
+
+	public float maxDegree = 45f;
+	public float input { get; set; }
+
+	// Use this for initialization
+	void Start()
+	{
+		input = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		propeller.Rotate(axis, angle);
+
+		if (input < 0)
+		{
+			roll = Mathf.MoveTowardsAngle(roll, -1f, speed * Time.deltaTime);
+		}
+		else if(input > 0)
+		{
+			roll = Mathf.MoveTowardsAngle(roll, 1f, speed * Time.deltaTime);
+		}
+		else
+		{
+			roll = Mathf.MoveTowardsAngle(roll, 0f, speed * Time.deltaTime);
+		}
+
+		rootNode.rotation = Quaternion.Euler(0, 0, roll * maxDegree * -1f);
+	}
+}
---
+++ stg00/Assets/EagleGames.jp/Scripts/Pl/PlCtrl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlCtrl : MonoBehaviour
+{
+	public float speed = 1f;
+	public P47Animation anim;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var dir = Vector3.zero;
+
+		if (Input.GetKey(KeyCode.UpArrow) == true)
+		{
+			dir.z = 1f;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow) == true)
+		{
+			dir.z = -1f;
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow) == true)
+		{
+			dir.x = -1f;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow) == true)
+		{
+			dir.x = 1f;
+		}
+
+		anim.input = dir.x;
+
+		transform.Translate(dir.normalized * Time.deltaTime * speed);
+	}
+}
